07140a585b53677d87833753f42400b9ceb6c60d
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-12-20 01:43:30 -0800

    feat(src): add pluggable error handlers to error handling middleware

--- src/backend/middleware/handle-error.ts
+++ src/backend/middleware/handle-error.ts
@@ -4,11 +4,8 @@ import { debugFactory } from 'multiverse/debug-extended';
 import {
   GuruMeditationError,
   NotFoundError,
-  ItemNotFoundError,
-  NotAuthorizedError,
-  InvalidIdError,
-  InvalidKeyError,
   ValidationError,
+  AuthError,
   AppError
 } from 'universe/error';
 
@@ -22,22 +19,41 @@ import {
 import type { JsonError } from '@xunnamius/types';
 import type { MiddlewareContext } from 'multiverse/next-api-glue';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import type { Promisable } from 'type-fest';
 
 const debug = debugFactory(`${debugNamespace}:glue:handle-error`);
 
+/**
+ * Special middleware used to handle custom errors.
+ */
+export type ErrorHandler = (
+  res: NextApiResponse,
+  errorJson: Partial<JsonError>
+) => Promisable<void>;
+
+/**
+ * A Map of Error class constructors to the special middleware that handles
+ * them.
+ */
+export type ErrorHandlerMap = Map<new (...args: any[]) => Error, ErrorHandler>;
+
 export type Options = {
-  // No options
+  /**
+   * A mapping of Error classes and the functions that handle them.
+   */
+  errorHandlers?: ErrorHandlerMap;
 };
 
 export default async function (
   _req: NextApiRequest,
   res: NextApiResponse,
-  context: MiddlewareContext
+  context: MiddlewareContext<Options>
 ) {
   debug('entered middleware runtime');
 
   const {
-    runtime: { error }
+    runtime: { error },
+    options: { errorHandlers }
   } = context;
 
   if (res.writableEnded) {
@@ -53,19 +69,31 @@ export default async function (
 
   debug('handling error: %O', errorJson.error || '(no message)');
 
+  if (errorHandlers) {
+    for (const [errorType, errorHandler] of errorHandlers) {
+      if (error instanceof errorType) {
+        debug(`using custom error handler for type "${error.name}"`);
+        await errorHandler(res, errorJson);
+        return;
+      }
+    }
+  }
+
+  debug(
+    `using default error handler${
+      error instanceof Error ? ` for type "${error.name}"` : ''
+    }`
+  );
+
   if (error instanceof GuruMeditationError) {
     sendHttpError(res, {
       error: 'sanity check failed: please report exactly what you did just now!'
     });
-  } else if (
-    error instanceof InvalidIdError ||
-    error instanceof InvalidKeyError ||
-    error instanceof ValidationError
-  ) {
+  } else if (error instanceof ValidationError) {
     sendHttpBadRequest(res, errorJson);
-  } else if (error instanceof NotAuthorizedError) {
+  } else if (error instanceof AuthError) {
     sendHttpUnauthorized(res, errorJson);
-  } else if (error instanceof NotFoundError || error instanceof ItemNotFoundError) {
+  } else if (error instanceof NotFoundError) {
     sendHttpNotFound(res, errorJson);
   } else if (error instanceof AppError) {
     sendHttpError(res, errorJson);
--- test/backend/middleware/unit-handle-error.test.ts
+++ test/backend/middleware/unit-handle-error.test.ts
@@ -2,15 +2,26 @@ import { withMiddleware } from 'multiverse/next-api-glue';
 import { testApiHandler } from 'next-test-api-route-handler';
 import { itemFactory, noopHandler, wrapHandler } from 'testverse/setup';
 import { toss } from 'toss-expression';
-import handleError from 'universe/backend/middleware/handle-error';
+import handleError, {
+  Options,
+  ErrorHandler
+} from 'universe/backend/middleware/handle-error';
 
 import {
-  InvalidIdError,
-  InvalidKeyError,
   ValidationError,
+  InvalidIdError,
+  InvalidEnvironmentError,
+  InvalidConfigurationError,
+  InvalidParameterError,
+  InvalidTokenError,
+  AuthError,
+  NotAuthenticatedError,
   NotAuthorizedError,
   NotFoundError,
   ItemNotFoundError,
+  HttpError,
+  TrialError,
+  DummyError,
   AppError,
   GuruMeditationError
 } from 'universe/error';
@@ -19,13 +30,21 @@ it('sends correct HTTP error codes when certain errors occur', async () => {
   expect.hasAssertions();
 
   const factory = itemFactory<[AppError | string, number]>([
-    [new InvalidIdError(), 400],
-    [new InvalidKeyError(), 400],
     [new ValidationError(), 400],
     [new ValidationError(''), 400], // ! Edge case for code coverage
+    [new InvalidIdError(), 400],
+    [new InvalidEnvironmentError(), 400],
+    [new InvalidConfigurationError(), 400],
+    [new InvalidParameterError(), 400],
+    [new InvalidTokenError(), 400],
+    [new AuthError(), 403],
+    [new NotAuthenticatedError(), 403],
     [new NotAuthorizedError(), 403],
     [new NotFoundError(), 404],
     [new ItemNotFoundError(), 404],
+    [new HttpError(), 500],
+    [new TrialError(), 500],
+    [new DummyError(), 500],
     [new AppError(), 500],
     [new GuruMeditationError(), 500],
     [new Error(), 500], // ? Every other error type should return 500
@@ -73,6 +92,40 @@ it('throws without calling res.end if response is no longer writable', async () 
     },
     test: async ({ fetch }) => {
       expect((await fetch()).status).toBe(200);
+    }
+  });
+});
+
+it('supports pluggable error handlers', async () => {
+  expect.hasAssertions();
+
+  const MyError = class extends Error {};
+
+  await testApiHandler({
+    rejectOnHandlerError: true,
+    handler: withMiddleware<Options>(undefined, {
+      use: [
+        () => {
+          throw new MyError('bad bad not good');
+        }
+      ],
+      useOnError: [handleError],
+      options: {
+        errorHandlers: new Map([
+          [
+            MyError,
+            (res, errorJson) => {
+              res.status(200).send(errorJson);
+            }
+          ]
+        ])
+      }
+    }),
+    test: async ({ fetch }) => {
+      expect((await fetch()).status).toBe(200);
+      await expect((await fetch()).json()).resolves.toStrictEqual({
+        error: 'bad bad not good'
+      });
     }
   });
 });
