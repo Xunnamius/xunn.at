5a1fc2f187fc2e4164db7c3aafcd5f1de60877cc
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-12-20 01:46:50 -0800

    refactor(src): use latest named-app-errors locally

--- src/error.ts
+++ src/error.ts
@@ -1,8 +1,9 @@
-// ? Errors specific to this application are defined here.
-
 //import { AppError, makeNamedError } from 'named-app-errors';
+
+export * from 'named-app-errors';
+
+// ? Errors specific to this application are defined here.
 
 //export class MyError extends AppError {}
 //makeNamedError(MyError, 'MyError');
-
 export {};
