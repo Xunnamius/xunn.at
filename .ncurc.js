0f77db1cb8b444519f0d16791f891db90ecd8e5e
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-12-28 20:13:01 -0800

    build(package): pin cjs version of find-up

--- .ncurc.js
+++ .ncurc.js
@@ -1,11 +1,16 @@
 // * https://www.npmjs.com/package/npm-check-updates#configuration-files
 
+// TODO: remove outdated deps and exceptions (below) when externalizing libs to
+// TODO: packages
+
 module.exports = {
   reject: [
     // ? Pin the CJS version of execa
     'execa',
     // ? Pin the CJS version of node-fetch (and its types)
     'node-fetch',
-    '@types/node-fetch'
+    '@types/node-fetch',
+    // ? Pin the CJS version of find-up
+    'find-up'
   ]
 };
