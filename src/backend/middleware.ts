9d2670a6b6e7bd878d9c2742ad12716dda741329
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2022-06-26 10:50:23 -0700

    refactor(src): add /sys privileged endpoint middleware

--- src/backend/middleware.ts
+++ src/backend/middleware.ts
@@ -4,6 +4,10 @@ import logRequest, {
   Options as LogRequestOptions
 } from 'multiverse/next-adhesive/log-request';
 
+import authRequest, {
+  Options as AuthRequestOptions
+} from 'multiverse/next-adhesive/auth-request';
+
 import checkVersion, {
   Options as CheckVersionOptions
 } from 'multiverse/next-adhesive/check-version';
@@ -17,6 +21,10 @@ import limitRequest, {
 import checkMethod, {
   Options as CheckMethodOptions
 } from 'multiverse/next-adhesive/check-method';
+
+import checkContentType, {
+  Options as CheckContentTypeOptions
+} from 'multiverse/next-adhesive/check-content-type';
 
 import handleError, {
   Options as HandleErrorOptions
@@ -39,7 +47,31 @@ const withMiddleware = middlewareFactory<
     HandleErrorOptions
 >({
   use: [logRequest, checkVersion, useCors, limitRequest, checkMethod],
-  useOnError: [handleError]
+  useOnError: [handleError],
+  options: {}
 });
 
-export { withMiddleware };
+/**
+ * Middleware runner for the special /sys API endpoints. Decorates a request
+ * handler.
+ *
+ * Passing `undefined` as `handler` or not calling `res.end()` (and not sending
+ * headers) in your handler or use chain will trigger an `HTTP 501 Not
+ * Implemented` response. This can be used to to stub out endpoints and their
+ * middleware for later implementation.
+ */
+/* istanbul ignore next */
+const withSysMiddleware = middlewareFactory<
+  LogRequestOptions &
+    AuthRequestOptions &
+    LimitRequestOptions &
+    CheckMethodOptions &
+    CheckContentTypeOptions &
+    HandleErrorOptions
+>({
+  use: [logRequest, authRequest, limitRequest, checkMethod, checkContentType],
+  useOnError: [handleError],
+  options: {}
+});
+
+export { withMiddleware, withSysMiddleware };
